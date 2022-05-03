df93a68d08b34e0e1540d70a3b277e9c82f97ec0
dorin <dorin@dorin.dorin>
2022-05-03 22:52:30 +0300

    #20220503_12: main script

--- ro_interview_assignment/main.py
+++ ro_interview_assignment/main.py
@@ -1,8 +1,12 @@
 import requests
+import ipaddress
 import json
 import time
 
+
 from data_structures.datacenter import Datacenter
+from data_structures.entry import Entry
+from data_structures.network_collection import NetworkCollection
 
 
 URL = "http://www.mocky.io/v2/5e539b332e00007c002dacbe"
@@ -55,7 +59,28 @@ def main():
         for key, value in data.items()
     ]
 
-    pass  # the rest of your logic here
+    # remove invalid clusters from datacenters
+    for datacenter in datacenters:
+        datacenter.remove_invalid_clusters()
+
+    # create network collections
+    network_collections = []
+    for datacenter in datacenters:
+        for cluster in datacenter.clusters:
+            networks_from_cluster = datacenter.clusters[cluster]["networks"]
+            for network in networks_from_cluster:
+                entry_list = []
+                for entry in networks_from_cluster[network]:
+                    entry_list.append(Entry(entry["address"], entry["available"], entry["last_used"]))
+                network_collections.append(NetworkCollection(network, entry_list))
+
+    # remove invalid records from network_collections
+    for network in network_collections:
+        network.remove_invalid_records()
+
+    # sort records from network_collections
+    for network in network_collections:
+        network.sort_records()
 
 
 if __name__ == '__main__':
