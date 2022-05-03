89fd0f0fd246d269b0feebfe394224461bf478c8
dorin <dorin@dorin.dorin>
2022-05-03 13:40:20 +0300

    #20220503_01: implementation of get_data

--- ro_interview_assignment/main.py
+++ ro_interview_assignment/main.py
@@ -1,3 +1,7 @@
+import requests
+import json
+import time
+
 from data_structures.datacenter import Datacenter
 
 
@@ -8,7 +12,7 @@ def get_data(url, max_retries=5, delay_between_retries=1):
     """
     Fetch the data from http://www.mocky.io/v2/5e539b332e00007c002dacbe
     and return it as a JSON object.
-​
+
     Args:
         url (str): The url to be fetched.
         max_retries (int): Number of retries.
@@ -16,7 +20,24 @@ def get_data(url, max_retries=5, delay_between_retries=1):
     Returns:
         data (dict)
     """
-    pass  # the rest of your logic here
+
+    data = {}
+
+    trial = 0
+
+    while True:
+        try:
+            response = requests.get(url)
+            if response.status_code != 200:
+                raise Exception
+            else:
+                data = json.loads(response.text)
+                return data
+        except Exception as e:
+            if trial + 1 > max_retries - 1:
+                return data
+            trial = trial + 1
+            time.sleep(delay_between_retries)
 
 
 def main():
