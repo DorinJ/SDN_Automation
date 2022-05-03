4a734d263194e54e93c7bff1720805cee0b150d4
dorin <dorin@dorin.dorin>
2022-05-03 19:01:49 +0300

    #20220503_07: implementation of remove_invalid_records method from NetworkCollection class

--- ro_interview_assignment/data_structures/network_collection.py
+++ ro_interview_assignment/data_structures/network_collection.py
@@ -18,7 +18,32 @@ class NetworkCollection:
         Removes invalid objects from the entries list.
         """
 
-        pass
+        ipv4_network = ipaddress.ip_network(self.ipv4_network)
+        raw_entry_list = self.raw_entry_list
+
+        bad_ip_entry = []
+
+        # mark bad entries and keep only network's IPv4 addresses
+        index = 0
+        for entry in raw_entry_list:
+            try:
+                ip = ipaddress.ip_address(entry["address"])
+                if ip in ipv4_network:
+                    pass
+                else:
+                    raise Exception
+            except Exception as e:
+                bad_ip_entry.append(index)
+            index = index + 1
+
+        # delete bad entries
+        while len(bad_ip_entry) > 0:
+            del raw_entry_list[bad_ip_entry[0]]
+            del bad_ip_entry[0]
+            i = 0
+            for x in bad_ip_entry:
+                bad_ip_entry[i] = x - 1
+                i = i + 1
 
     def sort_records(self):
         """
