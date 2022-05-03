ab9ca8fbfef543b2889d298af8d1d43bfd4e08db
dorin <dorin@dorin.dorin>
2022-05-03 14:06:48 +0300

    #20220503_04: constructor completion for Cluster class; mismatch between docstrings and constructor definition, question to be addressed

--- ro_interview_assignment/data_structures/cluster.py
+++ ro_interview_assignment/data_structures/cluster.py
@@ -1,11 +1,16 @@
+from ro_interview_assignment.data_structures.network_collection import NetworkCollection
+
+
 class Cluster:
-    def __init__(self, name, network_dict, security_level):
+    def __init__(self, name: str, network_dict: NetworkCollection, security_level: int):
         """
         Constructor for Cluster data structure.
 
         self.name -> str
+        self.network_dict -> list(NetworkCollection)
         self.security_level -> int
-        self.networks -> list(NetworkCollection)
         """
 
-        pass
+        self.name = name
+        self.network_dict = network_dict
+        self.security_level = security_level
