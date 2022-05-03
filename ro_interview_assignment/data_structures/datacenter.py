6d2ee211b47e9976c033c030f2f87d0baf33a9b7
dorin <dorin@dorin.dorin>
2022-05-03 15:03:00 +0300

    #20220503_06: implementation of remove_invalid_clusters method from Datacenter class; mismatch between docstrings and constructor definition, question to be addressed

--- ro_interview_assignment/data_structures/datacenter.py
+++ ro_interview_assignment/data_structures/datacenter.py
@@ -1,8 +1,10 @@
 from ro_interview_assignment.data_structures.cluster import Cluster
 
+import re
+
 
 class Datacenter:
-    def __init__(self, name: str, cluster_dict: Cluster):
+    def __init__(self, name: str, clusters: Cluster):
         """
         Constructor for Datacenter data structure.
 
@@ -11,11 +13,35 @@ class Datacenter:
         """
 
         self.name = name
-        self.cluster_dict = cluster_dict
+        self.clusters = clusters
 
     def remove_invalid_clusters(self):
         """
         Removes invalid objects from the clusters list.
         """
 
-        pass
+        bad_names = []
+
+        # mark bad names
+        for cluster in self.clusters:
+            try:
+                cluster_name = str(cluster).split("-")
+                if len(cluster_name) == 2:
+                    pattern = r'[A-Z]'
+                    if re.match(pattern, cluster_name[0]) and len(cluster_name[0]) == 3:
+                        pass
+                    else:
+                        raise Exception
+                    pattern = r'[0-9]'
+                    if re.match(pattern, cluster_name[1]) and 0 < int(cluster_name[1]) < 1000:
+                        pass
+                    else:
+                        raise Exception
+                else:
+                    raise Exception
+            except Exception as e:
+                bad_names.append(cluster)
+
+        # delete clusters indicated by bad_names list
+        for key in bad_names:
+            del self.clusters[key]
